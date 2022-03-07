ee475ea3cd63371376764c5750a8ec8401176ddf
kinasha <znella@foxmail.com>
2022-03-07 18:14:57 +0800

    add head

--- src/pages/content/headerPic/headerPic.jsx
+++ src/pages/content/headerPic/headerPic.jsx
@@ -1,5 +1,13 @@
 import './index.scss';
+import { LeftOutlined } from '@ant-design/icons';
 function HeaderPic() {
-  return <div className="headerPic">this is hP</div>;
+  return (
+    <div className="headerPic">
+      <div>
+        <LeftOutlined style={{ fontSize: '25px' }} />
+        <span style={{ fontSize: '18px' }}>愿望清单</span>
+      </div>
+    </div>
+  );
 }
 export default HeaderPic;
