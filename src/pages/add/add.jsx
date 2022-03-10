875dc2a671327590a3e98407a0c49436ae00a01e
kinasha <znella@foxmail.com>
2022-03-10 21:10:49 +0800

    add page add

--- src/pages/add/add.jsx
+++ src/pages/add/add.jsx
@@ -4,13 +4,23 @@ import { Input } from 'antd';
 
 export function Add() {
   const { TextArea } = Input;
-
+  const [todoContent, setTodoContent] = useState('');
   return (
     <div className="add">
-      <div className="headerPic">
-        <div className="headerTop">
-          <span>取消</span>
-          <span style={{ fontSize: '18px' }}>愿望清单</span>
+      <div className="add_headerPic">
+        <div className="add_headerTop">
+          <span
+            style={{ fontSize: '18px' }}
+            onClick={() => (window.location.href = '/Content')}
+          >
+            取消
+          </span>
+          <span
+            style={{ fontSize: '18px' }}
+            onClick={() => console.log(todoContent)}
+          >
+            确定
+          </span>
         </div>
         <TextArea
           showCount
@@ -18,6 +28,8 @@ export function Add() {
           placeholder="将你的愿望说给云听..."
           bordered={false}
           style={{ backgroundColor: 'transparent' }}
+          value={todoContent}
+          onChange={(e) => setTodoContent(e.target.value)}
         />
       </div>
     </div>
